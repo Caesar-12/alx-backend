04db8165ba25a19fa03be23962712b75a9aa0299
Caesar-12 <idowuenoch65@gmail.com>
2023-10-28 03:29:54 +0000

    Modfying cache methods

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -14,13 +14,19 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """Modified put method"""
 
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            for key in self.cache_data.keys():
-                discarded = self.cache_data.pop(key)
-                break
-            print("DICARD: {}".format(discarded[0]))
         if not key or not item:
             return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key in self.cache_data.keys():
+                self.cache_data[key] = item
+                return
+            else:
+                for keys in self.cache_data.keys():
+                    rem_key = keys
+                    del self.cache_data[keys]
+                    break
+                print("DICARD: {}".format(rem_key))
         self.cache_data[key] = item
 
     def get(self, key):
