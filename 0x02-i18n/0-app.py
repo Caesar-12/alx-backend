ddb5315e7f4e0c83a4338d73bcef9d5608e6eb4b
Caesar-12 <idowuenoch65@gmail.com>
2023-10-31 11:38:57 +0000

    Edit to pep8

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Flask"""
 from flask import Flask, render_template
-
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def welcome():
+    """homepage"""
     return render_template('0-index.html')
