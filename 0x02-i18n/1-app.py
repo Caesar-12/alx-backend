a35d0e3b2a92e0d43fc24bf1da82e94f64c56b43
Caesar-12 <idowuenoch65@gmail.com>
2023-10-31 12:30:20 +0000

    Configuration

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,10 +4,11 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
-class Config:
+class Config(Babel):
     """Config class for config"""
     LANGUAGES = ["en", "fr"]
-    TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
