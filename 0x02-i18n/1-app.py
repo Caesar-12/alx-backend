8b60d84d787987b088ccea4113b7537bdc590c4f
Caesar-12 <idowuenoch65@gmail.com>
2023-10-31 11:40:56 +0000

    Edit to pep8

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask babel"""
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
@@ -14,3 +14,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
+
+@app.route('/')
+def welcome():
+    """homepage"""
+    return render_template('0-index.html')
