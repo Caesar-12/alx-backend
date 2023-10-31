25259b400f3132352bb22a7d9731384f71c81969
Caesar-12 <idowuenoch65@gmail.com>
2023-10-31 11:36:24 +0000

    Setting up bael

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Flask"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('0-index.html')
