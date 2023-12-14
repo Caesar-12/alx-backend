f02e1804eec060f47f56afed948cc4f2ed1ac800
Caesar-12 <idowuenoch65@gmail.com>
2023-12-14 01:04:01 +0000

    Pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 """Contains a function that indexes"""
+from typing import Tuple
 
-def index_range(page, page_sixe):
-    pass
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    start index and an end index corresponding to the range of
+    """
+
+    return ((page-1) * page_size, page_size * page)
