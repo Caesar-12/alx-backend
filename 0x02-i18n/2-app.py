a957a864c2e2b82b1aae51fb10055a05594c7ae9
Caesar-12 <idowuenoch65@gmail.com>
2023-12-22 00:55:50 +0000

    Translations

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,27 +1,4 @@
 #!/usr/bin/env python3
-"""Flask babel"""
-from flask import Flask, render_template, request
-from flask_babel import *
-
-
-class Config(Babel):
-    """Config class for config"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-babel = Babel(app)
-
-
-@app.route('/')
-def welcome():
-    """homepage"""
-    return render_template('0-index.html')
-#!/usr/bin/env python3
 """A Basic Flask app.
 """
 from flask_babel import Babel
